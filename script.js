02162ef25e70bc708ea972499033994a7ecd03d2
sandro antadze <sandroiashvili169@gmail.com>
2024-10-21 01:29:39 +0400

    js davalebaaaaaaa

---
+++ script.js
@@ -0,0 +1,45 @@
+
+//1)
+
+let num = 20
+let square = num * num;
+
+if (square > 100) {
+console.log ("მეტია ასზე");
+} else {
+console.log("ნაკლებია ასზე")
+
+}
+
+//2)
+
+let number = 70; 
+if (number > 50 && number <100) {
+console.log("რიცხვი 50-ზე მეტია და 100ზე ნაკლები")
+
+}else{
+console.log("რიცხვი არ არის 50ზე მეტი ან 100ზე ნაკლები")
+}
+
+//3)
+
+let ricxvi = -30;
+if (number <=0){
+console.log("რიცხვი ნულის ტოლია ან უარყოფითი")
+}else{
+console.log("რიცხვი დადებითია")
+}
+
+//4)
+
+
+let lenght = 55;
+let width = 40;
+let perimeter = 2 * (lenght + width);
+
+if (perimeter > 50){
+    console.log("პერიმეტრი მეტია 50ზე");
+}else{
+console.log("პერიმეტრი ნაკლებია 50-ზე")
+
+}
